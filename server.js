85852a35136b6dc6f39892c81f0d76e0f0693f2e
Will Darkins <willdarkins@gmail.com>
2021-10-16 17:37:17 -0700

    Filtered query function made for specific notes

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ const { notes } = require('./db/db.json')
 
 app.use(express.static('public'));
 
+const filterByQuery = (query, notesArray) => {
+    let filteredResults = notesArray;
+    if(query.title) {
+        filteredResults = filteredResults.filter(note => note.title === query.title)
+    }
+    if(query.text) {
+        filteredResults = filteredResults.filter(note => note.text === query.text)
+    }
+    return filteredResults
+}
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
   });
@@ -20,13 +31,16 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'notes.html'))
   })
 
+/*Function takes in req.query as an argument and filters through the notes accordingly
+returning the new filtered array*/
 app.get('/api/notes', (req, res) => {
     console.log(notes)
-    res.json(notes);
+    let results = notes;
+    if(req.query) {
+        results = filterByQuery(req.query, results)
+    }
+    res.json(results);
 });
-
-
-
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
